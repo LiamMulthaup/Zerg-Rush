cb24c38e5346c57f10552485512164371698414c
Liam Multhaup <Liam@jmulthaup.com>
2016-06-13 10:58:26 -0700

    Added CreateAlien and KilllAlien methods. Also create alien struct.
    CreateAlien creates an alien picture box and new instance of the alien
    struct and adds both to a list.

---
+++ Zerg Rush/Zerg Rush/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Zerg_Rush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.defaultalienPicture = new System.Windows.Forms.PictureBox();
+            this.runTicker = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.defaultalienPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultalienPicture
+            // 
+            this.defaultalienPicture.Image = global::Zerg_Rush.Properties.Resources.f3b38e0625012600f8c71693e75443dc526c1;
+            this.defaultalienPicture.Location = new System.Drawing.Point(62, 30);
+            this.defaultalienPicture.Name = "defaultalienPicture";
+            this.defaultalienPicture.Size = new System.Drawing.Size(30, 25);
+            this.defaultalienPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.defaultalienPicture.TabIndex = 0;
+            this.defaultalienPicture.TabStop = false;
+            this.defaultalienPicture.Visible = false;
+            this.defaultalienPicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // runTicker
+            // 
+            this.runTicker.Tick += new System.EventHandler(this.runTicker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(924, 481);
+            this.Controls.Add(this.defaultalienPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.defaultalienPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox defaultalienPicture;
+        private System.Windows.Forms.Timer runTicker;
+    }
+}
+
---
+++ Zerg Rush/Zerg Rush/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zerg_Rush
+{
+    public partial class Form1 : Form
+    {
+        struct aliens
+        {
+            public int position;
+            public int health;
+        }
+        List<aliens> alienList = new List<aliens>();
+        public Form1()
+        {
+            InitializeComponent();
+            runTicker.Start();
+        }
+        List<PictureBox> alienPictureList = new List<PictureBox>();
+        int gamephase = 0;
+        List<int> createAlienWaitTimers = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            gamephase = 1;
+            createAlienWaitTimers.Add(50 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(60 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(70 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(80 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(90 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(100 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(110 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(120 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(130 + rand.Next(rand.Next(8)));
+            createAlienWaitTimers.Add(140 + rand.Next(rand.Next(8)));
+        }
+
+        private void runTicker_Tick(object sender, EventArgs e)
+        {
+            if (gamephase == 1)
+            {
+                for (int x = 0; x < alienList.Count; x++)
+                {
+
+                }
+                for (int x = createAlienWaitTimers.Count - 1; x >= 0; x--)
+                {
+                    createAlienWaitTimers[x] = createAlienWaitTimers[x] - 1;
+                    if (createAlienWaitTimers[x] < 1)
+                    {
+                        createAlien();
+                        createAlienWaitTimers.RemoveAt(x);
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (sender != defaultalienPicture)
+            {
+                killAlien(sender);
+            }
+            else
+            {
+                createAlien();
+            }
+        }
+        private void createAlien()
+        {
+            Random rand = new Random();
+            aliens alien = new aliens();
+            alien.health = rand.Next(2 + rand.Next(3));
+            alien.position = 0;
+            alienList.Add(alien);
+            PictureBox alienPicture = new PictureBox();
+            alienPicture.Click += pictureBox1_Click;
+            alienPicture.Image = Properties.Resources.f3b38e0625012600f8c71693e75443dc526c1;
+            alienPicture.Size = defaultalienPicture.Size;
+            alienPicture.SizeMode = PictureBoxSizeMode.Zoom;
+            alienPictureList.Add(alienPicture);
+            Controls.Add(alienPictureList[alienPictureList.Count - 1]);
+            alienPictureList[alienPictureList.Count - 1].Location = new Point(rand.Next(940), rand.Next(520));
+        }
+        private void killAlien(object sender)
+        {
+            PictureBox pbx = sender as PictureBox;
+            alienList.RemoveAt(alienPictureList.IndexOf(pbx));
+            alienPictureList.Remove(pbx);
+            Controls.Remove(pbx);
+            pbx.Dispose();
+        }
+    }
+}
